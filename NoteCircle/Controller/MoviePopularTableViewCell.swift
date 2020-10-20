d1dfb780a6a846e72b18d9b16199f2da46677c69
Karthik Rajan T  <karthikrajant@Karthiks-MacBook-Pro.local>
2020-10-20 17:01:16 +0530

    TableViewSection

---
+++ NoteCircle/Controller/MoviePopularTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  MoviePopularTableViewCell.swift
+//  NoteCircle
+//
+//  Created by Karthik Rajan T  on 20/10/20.
+//  Copyright © 2020 Karthik Rajan T . All rights reserved.
+//
+
+import UIKit
+
+class MoviePopularTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var popularMovieImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- NoteCircle/Controller/MovieViewController.swift
+++ NoteCircle/Controller/MovieViewController.swift
@@ -2,92 +2,48 @@
 //  MovieViewController.swift
 //  NoteCircle
 //
-//  Created by Karthik Rajan T  on 19/10/20.
+//  Created by Karthik Rajan T  on 20/10/20.
 //  Copyright © 2020 Karthik Rajan T . All rights reserved.
 //
 
 import UIKit
 
 class MovieViewController: UIViewController {
+    // MARK: - Properties
+    @IBOutlet weak var movieTableView: UITableView!
     
-    @IBOutlet weak var popularMovieCollectionView: UICollectionView!
-    @IBOutlet weak var movieEventCollectionView: UICollectionView!
+    var items: [String] = ["Item1", "Item2", "Item3", "Item4"]
+    var movieImage: [UIImage] = [
+        UIImage(named: "item1")!,UIImage(named: "item2")!,UIImage(named: "item3")!,UIImage(named: "item4")!
+    ]
     
-    private let apiURL = "https://api.themoviedb.org/3/5afb74f0331b3a1755910a738c87a133"
-    private let api_key = "5afb74f0331b3a1755910a738c87a133"
-    private let imageURL = "https://image.tmdb.org/t/p/"
-    private var images = [ImageModel]()
-    
+    // MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        popularMovieCollectionView.delegate = self
-        popularMovieCollectionView.dataSource = self
-        
-        movieEventCollectionView.delegate = self
-        movieEventCollectionView.dataSource = self
+        setupView()
         
     }
-    func getLatestImage() {
-        guard let movieURL = URL(string: apiURL) else {
-            return
-        }
+    // MARK: - View Methods
+    fileprivate func setupView() {
+        movieTableView.register(UINib(nibName: "MoviePopularTableViewCell", bundle: nil), forCellReuseIdentifier: "Cell")
         
-        let request = URLRequest(url: movieURL)
-        let task = URLSession.shared.dataTask(with: request, completionHandler: { (data, response, error) -> Void in
-            
-            if let error = error {
-                print(error)
-                return
-            }
-            
-            if let data = data {
-                self.images = self.parseJsonData(data: data)
-                
-                OperationQueue.main.addOperation({
-                    self.popularMovieCollectionView.reloadData()
-                    self.movieEventCollectionView.reloadData()
-                })
-            }
-        })
-        
-        task.resume()
-    }
-    func parseJsonData(data: Data) -> [ImageModel] {
-        
-        var images = [ImageModel]()
-        
-        do {
-            let jsonResult = try JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions.mutableContainers) as? NSDictionary
-            
-            let jsonImages = jsonResult?["images"] as! [AnyObject]
-            for jsonImage in jsonImages {
-                var image = ImageModel()
-                
-                image.name = jsonImage["name"] as! String
-                image.image = jsonImage["use"] as! String
-                
-                images.append(image)
-            }
-            
-        } catch {
-            print(error)
-        }
-        
-        return images
     }
     
     
 }
-extension MovieViewController: UICollectionViewDelegate, UICollectionViewDataSource {
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 1
+// MARK: - UITableViewDataSource
+extension MovieViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return movieImage.count
     }
     
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! MovieEventCollectionViewCell
-        return cell
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MoviePopularTableViewCell", for: indexPath) as! MoviePopularTableViewCell
+        cell.label.text = items[indexPath.row]
+        cell.popularMovieImageView.image = movieImage[indexPath.row]
         
+        return cell
     }
-    
     
 }
